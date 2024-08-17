4c946202f838022e0948b88f5708b11e3478a73f
Venkatesh Alampally <rudhranethra@Venkateshs-MacBook-Air.local>
2024-08-17 21:59:04 +0530

    Initialized

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+
+function App() {
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      
+    </Box>
+  </BrowserRouter>;
+}
+
+export default App;
