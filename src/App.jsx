334541c87e962bcaf9ceb9d1ba1e887ba305fbf6
--global <--global>
2024-08-18 16:01:33 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
+import {
+  NavBar,
+  Feed,
+  VideoDetail,
+  SearchFeed,
+  ChannelDetail,
+} from "./components";
 
 function App() {
-  <BrowserRouter>
-    <Box sx={{ backgroundColor: "#000" }}>
-      
-    </Box>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <NavBar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchterm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/SearchFeed.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SearchFeed = () => {
+  return <divSearchFeed>SearchFeed</divSearchFeed>;
+};
+
+export default SearchFeed;
