78a4cb6f3f132ab758b356348d747d2e03951ec0
Venki1402 <saivenkatesh.alampally@gmail.com>
2024-10-15 21:59:51 +0530

    bug fix
    Signed-off-by: Venki1402 <saivenkatesh.alampally@gmail.com>

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -2,15 +2,17 @@ import { useState, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
 import { Videos } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { useParams } from "react-router-dom";
 
 const SearchFeed = () => {
   const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${}`).then((data) => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) => {
       setVideos(data.items);
     });
-  }, []);
+  }, [searchTerm]);
 
   return (
     <Box
@@ -22,7 +24,7 @@ const SearchFeed = () => {
       }}
     >
       <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-        {selectedCategory} <span style={{ color: "#F31503" }}>videos</span>
+        Search results for: <span style={{ color: "#F31503" }}>videos</span>
       </Typography>
       <Videos videos={videos} />
     </Box>
